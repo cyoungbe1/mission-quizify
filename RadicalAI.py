f713f8474bc0ee930b71bd3d4b8deac82eb55f43
cyoungbe1 <cyoungbe1@gmail.com>
2024-04-09 21:30:49 -0400

    Initial commit with RadicalAI.py file

---
+++ RadicalAI.py
@@ -0,0 +1,30 @@
+from langchain_google_vertexai import VertexAIEmbeddings
+
+class EmbeddingClient:
+    def __init__(self, model_name, project, location):
+        # Initialize the VertexAIEmbeddings client with the given parameters
+        self.client = VertexAIEmbeddings(model_name=model_name, project=project, location=location)
+
+    def embed_query(self, query):
+        """Uses the embedding client to retrieve embeddings for the given query."""
+        vectors = self.client.embed_query(query)
+        return vectors
+
+    def embed_documents(self, documents):
+        """Retrieve embeddings for multiple documents."""
+        try:
+            return self.client.embed_documents(documents)
+        except AttributeError:
+            print("Method embed_documents not defined for the client.")
+            return None
+
+if __name__ == "__main__":
+    model_name = "textembedding-gecko@003"
+    project = "YOUR PROJECT ID HERE"
+    location = "us-central1"
+
+    embedding_client = EmbeddingClient(model_name, project, location)
+    vectors = embedding_client.embed_query("Hello World!")
+    if vectors:
+        print(vectors)
+        print("Successfully used the embedding client!")
